4d2c6cbde6715cb286960aa2a9e3857066f73ad3
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-11-06 23:46:00 +0330

    build: downgrade to next 14
    due to dependency problem, downgrade to version 14 of nextjs

---
+++ src/provider/RootProvider.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/theme/defaultTheme';
+
+const RootProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
+};
+
+export default RootProvider;
