2116c85db5291a5d6cb24e5b717fee0279edbe38
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-11-07 19:26:06 +0330

    feat: add about me section
    create a section which will provide a summary about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import AboutMe from '@/components/Sections/AboutMe/AboutMe';
 import Introduction from '@/components/Sections/Introduction/Introduction';
+import { Container } from '@mui/material';
 
 export default function Home() {
   return (
-    <div>
-      <Introduction />
-    </div>
+    <Container maxWidth='lg' component={'main'}>
+      <>
+        <Introduction />
+        <AboutMe />
+      </>
+    </Container>
   );
 }
---
+++ src/components/Sections/AboutMe/AboutMe.tsx
@@ -0,0 +1,35 @@
+import { Box, Typography } from '@mui/material';
+import data from '@/Repository/content.json';
+import styles from './AboutMe.module.scss';
+import Image from 'next/image';
+import MyPhoto from '@/public/hb.png';
+
+export default function AboutMe() {
+  return (
+    <Box component='section' className={styles.sectionWrapper}>
+      <Typography component='h1' fontWeight='600' fontSize={'28px'} lineHeight={'36px'} margin={'2rem'}>
+        About Me
+      </Typography>
+      <Box className={styles.aboutMe}>
+        <Box className={styles.aboutMeWrapper}>
+          <Box className={styles.titleAndDescription}>
+            <Typography component='h2' fontWeight={'500'} lineHeight={'24px'}>
+              A little about me
+            </Typography>
+            <Typography fontWeight={'400'} lineHeight={'20px'} textAlign={'justify'} fontSize={'14px'}>
+              {data.aboutMe.description.map((item) => {
+                return (
+                  <>
+                    <br />
+                    <>{item}</>
+                  </>
+                );
+              })}
+            </Typography>
+          </Box>
+          <Image src={MyPhoto} width={200} height={200} alt='My-photo' className={styles.myPhoto} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
