5e425b7c957f342851935dca39b8a0adf0b9e41f
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-11-07 21:40:11 +0330

    chore: add content to json file
    add technical skills data to json file and render them on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 import AboutMe from '@/components/Sections/AboutMe/AboutMe';
 import Introduction from '@/components/Sections/Introduction/Introduction';
-import { Container } from '@mui/material';
-
+import { Box, Container } from '@mui/material';
+import data from '@/Repository/content.json';
+import SkillCard from '@/components/SkillCard/SkillCard';
 export default function Home() {
   return (
     <Container maxWidth='lg' component={'main'}>
       <>
         <Introduction />
         <AboutMe />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            columnGap: '1rem',
+            rowGap: '5rem',
+            mt: '4rem',
+          }}
+        >
+          {data.skills.technical.map((item) => {
+            return <SkillCard key={item.id} imageSrc='' title={item.name} width={103} height={100} />;
+          })}
+        </Box>
       </>
     </Container>
   );
