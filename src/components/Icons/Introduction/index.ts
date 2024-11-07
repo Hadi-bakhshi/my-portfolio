7d52edbbbf979d61412950f8a031059aec8cdbee
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-11-07 13:36:24 +0330

    feat: add new icons for introduction

---
+++ src/components/Icons/Introduction/index.ts
@@ -0,0 +1,5 @@
+export { default as CSharpIcon } from './CSharpIcon';
+export { default as DotnetIcon } from './DotnetIcon';
+export { default as NodeJsIcon } from './NodeJsIcon';
+export { default as ReactIcon } from './ReactIcon';
+export { default as SqlServerIcon } from './SqlServerIcon';
