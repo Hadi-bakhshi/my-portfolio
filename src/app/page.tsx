c58c4656d42e70e6abf842462919d84381b660c4
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-11-08 13:37:13 +0330

    feat: add skills section
    create new section which will show skills such as soft skills and
    technical

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
 import AboutMe from '@/components/Sections/AboutMe/AboutMe';
 import Introduction from '@/components/Sections/Introduction/Introduction';
-import { Box, Container } from '@mui/material';
-import data from '@/Repository/content.json';
-import SkillCard from '@/components/SkillCard/SkillCard';
+import { Container } from '@mui/material';
+import Skills from '@/components/Sections/Skills/Skills';
+
 export default function Home() {
   return (
     <Container maxWidth='lg' component={'main'}>
-      <>
-        <Introduction />
-        <AboutMe />
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            columnGap: '1rem',
-            rowGap: '5rem',
-            mt: '4rem',
-          }}
-        >
-          {data.skills.technical.map((item) => {
-            return <SkillCard key={item.id} imageSrc='' title={item.name} width={103} height={100} />;
-          })}
-        </Box>
-      </>
+      <Introduction />
+      <AboutMe />
+      <Skills />
     </Container>
   );
 }
