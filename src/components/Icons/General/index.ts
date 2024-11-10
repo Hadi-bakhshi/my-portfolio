fe0a85fd17ffd5d9a172cfaade0532a5d5acdeb4
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-11-10 23:33:17 +0330

    chore: add new icon

--- src/components/Icons/General/index.ts
+++ src/components/Icons/General/index.ts
@@ -1,2 +1,3 @@
-export { default as SearchIcon } from './SearchIcon';
+export { default as EyeIcon } from './EyeIcon';
 export { default as MoreDetailsIcon } from './MoreDetailsIcon';
+export { default as SearchIcon } from './SearchIcon';
