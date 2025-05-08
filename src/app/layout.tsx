4116f4267f99a92888cc5d42e85c31a0609ddca6
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-05-08 13:13:40 +0330

    feat: add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata, Viewport } from 'next';
-import './globals.scss';
-import RootProvider from '@/provider/RootProvider';
-import { Roboto } from 'next/font/google';
+import type { Metadata, Viewport } from "next";
+import "./globals.scss";
+import RootProvider from "@/provider/RootProvider";
+import { Roboto } from "next/font/google";
+import Script from "next/script";
 // const geistSans = localFont({
 //   src: './fonts/GeistVF.woff',
 //   variable: '--font-geist-sans',
@@ -13,32 +14,32 @@ import { Roboto } from 'next/font/google';
 //   weight: '100 900',
 // });
 const roboto = Roboto({
-  display: 'swap',
-  weight: ['100', '300', '400', '500', '700', '900'],
-  subsets: ['latin'],
+  display: "swap",
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Hadi Bakhshi | Software Engineer',
-  description: 'Hadi Bakhshi personal portfolio',
+  title: "Hadi Bakhshi | Software Engineer",
+  description: "Hadi Bakhshi personal portfolio",
   keywords: [
-    'Software engineer',
-    'Full-stack developer',
-    'Front-end developer',
-    '.Net Developer',
-    'Dotnet',
-    'Engineer',
-    'Developer',
-    'portfolio',
+    "Software engineer",
+    "Full-stack developer",
+    "Front-end developer",
+    ".Net Developer",
+    "Dotnet",
+    "Engineer",
+    "Developer",
+    "portfolio",
   ],
 };
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1.0,
   // maximumScale: 1.0,
   // userScalable: false,
   // viewportFit: 'cover',
-  themeColor: '#2151DA',
+  themeColor: "#2151DA",
 };
 
 export default function RootLayout({
@@ -47,7 +48,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
+      <head>
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+           (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rgac5i4b7x");
+        `}
+        </Script>
+      </head>
       <body className={roboto.className}>
         <RootProvider>{children}</RootProvider>
       </body>
